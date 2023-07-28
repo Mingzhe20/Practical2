622b5ce285984f0f5374d762b3a81c1dfba4bdf1
Ming Zhe <nmingzhe02@gmail.com>
2023-07-28 15:16:03 +0800

    added binding

--- app/src/main/java/com/example/practical2/MainActivity.kt
+++ app/src/main/java/com/example/practical2/MainActivity.kt
@@ -3,56 +3,86 @@ package com.example.practical2
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Nickname
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.practical2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var editText:EditText
+    lateinit var binding: ActivityMainBinding
+    private var myName2: MyName = MyName("BAIT")
+
+    lateinit var editText: EditText
     lateinit var nicknameTextView: TextView
-    lateinit var doneButton:Button
+    lateinit var doneButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText = findViewById(R.id.nickname_edit)
-        nicknameTextView=findViewById(R.id.nickname_text)
-        doneButton=findViewById(R.id.done_button)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName1 = myName2
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        editText = findViewById(R.id.nickname_edit)
+        nicknameTextView = findViewById(R.id.nickname_text)
+        doneButton = findViewById(R.id.done_button)
+
+
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
 
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener{
+
+        binding.nicknameText.setOnClickListener {
             updateNickname(it)
         }
     }
 
-    private fun addNickname(view:View){
+    private fun addNickname(view: View) {
 //        val editText:EditText = findViewById(R.id.nickname_edit)
 //        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text=editText.text
-        editText.visibility=View.GONE
-        view.visibility=View.GONE
-        nicknameTextView.visibility=View.VISIBLE
+//        binding.nicknameText.text=binding.nicknameEdit.text.toString()
+//        binding.nicknameEdit.visibility=View.GONE
+//        binding.doneButton.visibility=View.GONE
+//        binding.nicknameText.visibility=View.VISIBLE
+
+        binding.apply {
+            myName1?.nickname = binding.nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+//        nicknameTextView.text=editText.text
+//        editText.visibility=View.GONE
+//        view.visibility=View.GONE
+//        nicknameTextView.visibility=View.VISIBLE
 
         // Hide the keyboard.
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
 
-    private fun updateNickname(view: View){
+    private fun updateNickname(view: View) {
 //        val editText:EditText = findViewById(R.id.nickname_edit)
 //       val doneButton = findViewById<Button>(R.id.done_button)
 
-        editText.visibility=View.VISIBLE
-        doneButton.visibility=View.VISIBLE
-        view.visibility=View.GONE
+        binding.apply {
+            nicknameEdit.visibility = View.VISIBLE
+            doneButton.visibility = View.VISIBLE
+            nicknameText.visibility = View.GONE
+        }
+
+//        editText.visibility = View.VISIBLE
+//        doneButton.visibility = View.VISIBLE
+//        view.visibility = View.GONE
     }
 }
